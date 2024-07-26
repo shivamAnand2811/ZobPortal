7448aff20350fd691263435d60669f3823097e48
Shivam <shivamanand4273@gmail.com>
2024-07-26 18:29:13 +0530

    controllers and routes for company , job and user designed

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utills/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js"
+import jobRoute from "./routes/job.route.js";
+
+
 dotenv.config({});
 
 
@@ -21,13 +26,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http//localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true
 }
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+
+app.use("/api/v1/user",userRoute)
+app.use("/api/v1/company",companyRoute)
+app.use("/api/v1/job",jobRoute)
+
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at ${PORT}`);
