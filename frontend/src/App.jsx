574dc313623d07cdc8b24e820b8508dfa50c43a2
Shivam <shivamanand4273@gmail.com>
2024-07-28 23:17:01 +0530

    updating frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import  Home  from './components/Home'
+
+
+const appRouter =createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+
+  },
+  {
+    path:'/',
+    element:<Home/>
+
+  },
+  {
+    path:'/login',
+    element:<Login/>
+
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+
+  },
+   
+])
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <h1>lets build job portal</h1>
-    </>
+    <div>
+      <RouterProvider router ={appRouter}/>
+    </div>
   )
 }
 
---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel';
+import { Button } from './ui/button';
+
+
+const category = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "FullStack Developer"
+]
+
+const CategoryCarousel = () => {
+
+
+    return (
+        <div>
+            <Carousel className="w-full max-w-xl mx-auto my-20">
+                <CarouselContent>
+                    {
+                        category.map((cat, index) => (
+                            // eslint-disable-next-line react/jsx-key
+                            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                                <Button variant="outline" className="rounded-full">{cat}</Button>
+                            </CarouselItem>
+                        ))
+                    }
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
+    )
+}
+
+export default CategoryCarousel
